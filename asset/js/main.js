67b945ebedaf1aa01fe7329049661cb93c6faad8
Irvan Rifaí <ivan.rivai6921@gmail.com>
2023-04-26 10:56:37 +0700

    section navbar, slider-carousel, program, sambutan

--- asset/js/main.js
+++ asset/js/main.js
@@ -132,6 +132,26 @@
             }
         }
     });
+
+    // program carousel
+    $(".program-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        margin: 20,
+        dots: false,
+        loop: true,
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            }
+        }
+    });
     
 })(jQuery);
 
